fe4a3579b7010e536a9940f2b8cb307529c2f044
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2026-02-06 10:24:09 +0200

    simple  react learning project initialization

---
+++ simple-learning-project/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+const books = [
+  { title: 'Mastering React', author: 'Anthony Pham', cover: MasteringReact },
+  { title: 'Practical React', author: 'Alex Johnson', cover: PracticalReact },
+  { title: 'React in Action', author: 'Bob Climo', cover: ReactInAction },
+];
+function App() {
+
+
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App

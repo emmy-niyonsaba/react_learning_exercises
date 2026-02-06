2f56ab60aadd1ace7f4e2fe27b6147b0e299b1e0
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2026-02-06 10:53:15 +0200

    ref: simple project render the book form array

--- simple-learning-project/src/App.jsx
+++ simple-learning-project/src/App.jsx
@@ -1,18 +1,33 @@
 import React from 'react'
 import './App.css'
+
 const books = [
-  { title: 'Mastering React', author: 'Anthony Pham', cover: MasteringReact },
-  { title: 'Practical React', author: 'Alex Johnson', cover: PracticalReact },
-  { title: 'React in Action', author: 'Bob Climo', cover: ReactInAction },
+  { title: 'Mastering React', author: 'Anthony Pham', cover: "MasteringReact" },
+  { title: 'Practical React', author: 'Alex Johnson', cover: "PracticalReact" },
+  { title: 'React in Action', author: 'Bob Climo', cover: "The last book cover" },
 ];
+
 function App() {
 
+  const bookCard = () => {
+    return books.map((book, index) => (
+      <div key={index} className="book-card">
+        <h2>The book Collection</h2>
+        <h1>Book {index + 1}</h1>
+        <p>Name: {book.title}</p>
+        <p>Author: {book.author}</p>
+        <p>Cover: {book.cover}</p>
+      </div>
+    ));
+  };
 
   return (
     <>
-      <h1>Hello</h1>
+      <section className="book-list">
+        {bookCard()}
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

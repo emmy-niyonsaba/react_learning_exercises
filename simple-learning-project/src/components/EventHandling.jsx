ebcbf0b26f543b78bdd8cb60ac3321442ee54589
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2026-02-08 19:54:27 +0200

    add the state handling

--- simple-learning-project/src/components/EventHandling.jsx
+++ simple-learning-project/src/components/EventHandling.jsx
@@ -1,18 +1,32 @@
 
 
 
-import React from 'react'
+import React, { use } from 'react'
+import { useState } from 'react';
 
 const EventHandling = () => {
 
+    const [name, setName] =useState("Guest")
 
+
+
+    const handerSetName=()=>{
+        setName("Emmanuel");
+    }
     const handleClick = () => {
         alert("You have clicked the button")
     }
   return (
     <div>
       <h1>This section is to demonstrate event handling in React</h1>
+     
+        <br />
+        <br />
+            <h2>Welcome again {name}</h2>
+        
       <button onClick={handleClick}>ClickMe No</button>
+
+      <button onClick={handerSetName}>Say Hi!</button>
     </div>
   )
 }

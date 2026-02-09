58d42f8b9865bf44fc4534c2fb4c9c74896a19e9
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2026-02-09 10:29:39 +0200

    fix form handling

--- simple-learning-project/src/components/SimpleForm.jsx
+++ simple-learning-project/src/components/SimpleForm.jsx
@@ -3,41 +3,34 @@ import React from 'react'
 import { useState } from 'react'
 
 const SimpleForm = () => {
-  const [formData, setFormData] = useState({
+
+const [formData, setFormData] = useState({
     name: "",
     school: "",
     class: "ITA"
-  })
-  handlerChangename = () => {
-    setFormData({
-      name: formData.name,
-    })
-  }
-  handlerChangeschool = () => {
-    setFormData({
-      school: formData.school,
-    })
-  }
-  handlerChangeclass   = () => {
-    setFormData({
-      class: formData.class
-    })
-  }
-  handlerSumbit = (e) => {
+})
 
-      alert(`Name: ${formData.name}, School: ${formData.school}, Class: ${formData.class}`)
+
+const handlerChange = (e) => {
+  const [name, value] = e.target
+    setFormData({
+        ...formData,
+        [name]: value
+    })
+}
+const handlerSubmit = (e) => {
     e.preventDefault();
-  }  
-
+   alert(JSON.stringify(formData));
+}
   return (
     <div>
-      <form onSubmit={handlerSumbit} >
-        Name: <input type="text" name="name" className='name' placeholder='Enter name' onChange={handlerChangename}  value={formData.name}/>
-        School: <input type="text" name="school" className='school' placeholder='Enter school' onChange={handlerChangeschool} value={formData.school}/>
-        Class: <input type="text" name="class" className='class' placeholder='Enter class' onChange={handlerChangeclass} value={formData.class} />
+      <form onSubmit={handlerSubmit} >
+        Name: <input type="text" name="name" className='name' placeholder='Enter name'onChange={handlerChange} />
+        School: <input type="text" name="school" className='school' placeholder='Enter school'onChange={handlerChange} />
+        Class: <input type="text" name="class" className='class' placeholder='Enter class' onChange={handlerChange}/>
 
 
-    <button type='submit'>Save</button>
+          <button type="submit">Submit</button>
       </form>
     </div>
   )

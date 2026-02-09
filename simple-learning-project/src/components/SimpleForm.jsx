5ab7424a470f14359fcba21fac3e67080228dd68
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2026-02-09 10:13:36 +0200

    add the simple form handling with state

--- simple-learning-project/src/components/SimpleForm.jsx
+++ simple-learning-project/src/components/SimpleForm.jsx
@@ -1,13 +1,43 @@
 
 import React from 'react'
+import { useState } from 'react'
 
 const SimpleForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    school: "",
+    class: "ITA"
+  })
+  handlerChangename = () => {
+    setFormData({
+      name: formData.name,
+    })
+  }
+  handlerChangeschool = () => {
+    setFormData({
+      school: formData.school,
+    })
+  }
+  handlerChangeclass   = () => {
+    setFormData({
+      class: formData.class
+    })
+  }
+  handlerSumbit = (e) => {
+
+      alert(`Name: ${formData.name}, School: ${formData.school}, Class: ${formData.class}`)
+    e.preventDefault();
+  }  
+
   return (
     <div>
-      <form action="">
-        Name: <input type="text" name="name" className='name' placeholder='Enter name' />
-        School: <input type="text" name="school" className='school' placeholder='Enter school' />
-        Class: <input type="text" name="class" className='class' placeholder='Enter class' />
+      <form onSubmit={handlerSumbit} >
+        Name: <input type="text" name="name" className='name' placeholder='Enter name' onChange={handlerChangename}  value={formData.name}/>
+        School: <input type="text" name="school" className='school' placeholder='Enter school' onChange={handlerChangeschool} value={formData.school}/>
+        Class: <input type="text" name="class" className='class' placeholder='Enter class' onChange={handlerChangeclass} value={formData.class} />
+
+
+    <button type='submit'>Save</button>
       </form>
     </div>
   )

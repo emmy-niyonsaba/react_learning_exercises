3e8a8768eacea92ff4912da47549441c9b91ce6d
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2026-02-24 11:58:03 +0200

    add the new spinner

--- simple-learning-project/src/components/GetData.jsx
+++ simple-learning-project/src/components/GetData.jsx
@@ -1,23 +1,23 @@
-import {ClipLoader,BeatLoader} from 'react-spinners';
+import {ClipLoader, BeatLoader, ScaleLoader} from 'react-spinners';
 import useFetch from "../hooks/useFetch"
 const GetData = () => {
     const url = "https://jsonplaceholder.typicode.com/posts"
-  
+
     const { data, loading, error } = useFetch(url)
 
-  return (
-    <div>
-        {loading && <BeatLoader size={200} color={"#123abc"} />}
-        {error && <p>Error: {error.message}</p>}
-      {data&& data.map(item => (
-        <div key={item.id}>
-            <h2>{item.title}</h2>
-            <p>{item.body}</p>
+    return (
+        <div>
+            {loading && <ScaleLoader size={200} color={"#123abc"} />}
+            {error && <p>Error: {error.message}</p>}
+            {data && data.map(item => (
+                <div key={item.id}>
+                    <h2>{item.title}</h2>
+                    <p>{item.body}</p>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-   
-  )
+
+    )
 }
 
 export default GetData
